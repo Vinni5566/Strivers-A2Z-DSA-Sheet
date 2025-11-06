e7243f39a53e3a27f19daf4f5c5410e8256a8a79
Vinni Kapoor <vinni3871@gmail.com>
2025-11-06 20:59:22 +0530

    Add

---
+++ Dynamic Programming/2D_3D_DPandDP_ON_GRIDS/06_minimumFallingPath.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+//Approach - 1
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
+                if(i == 0) {
+                    dp[i][j] = matrix[i][j];
+                } else {
+                    int up = dp[i-1][j];
+                    int left = (j > 0)? dp[i-1][j-1] : INT_MAX;
+                    int right = (j < n-1)? dp[i-1][j+1] : INT_MAX;
+
+                    dp[i][j] = matrix[i][j] + min(min(up, left), right);
+                }
+            }
+        }
+
+        int minPath = dp[m-1][0];
+        for(int i = 1; i < m; i++) {
+            if(dp[m-1][i] < minPath) minPath = dp[m-1][i];
+        }
+
+        return minPath;
+    }
+};
+
+//Approach - 2
+class Solution { 
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < m; j++) {
+
+                if(i == 0) {
+                    dp[i][j] = matrix[i][j];
+                } else {
+                    if(j == 0) {
+                        dp[i][j] = matrix[i][j] + min(dp[i-1][j], dp[i-1][j+1]);
+                    } else if(j == n-1) {
+                        dp[i][j] = matrix[i][j] + min(dp[i-1][j], dp[i-1][j-1]);
+                    } else {
+                        dp[i][j] = matrix[i][j] + min(dp[i-1][j], min(dp[i-1][j-1], dp[i-1][j+1]));
+                    }
+                }
+            }
+        }
+
+        int minPath = dp[m-1][0];
+        for(int i = 1; i < m; i++) {
+            if(dp[m-1][i] < minPath) minPath = dp[m-1][i];
+        }
+
+        return minPath;
+    }
+};
