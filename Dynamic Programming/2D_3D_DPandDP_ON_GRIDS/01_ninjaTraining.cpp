cc9cb50f742649915d06370d2c8ac32e19f62b33
Vinni Kapoor <vinni3871@gmail.com>
2025-11-06 15:59:36 +0530

    Add

---
+++ Dynamic Programming/2D_3D_DPandDP_ON_GRIDS/01_ninjaTraining.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int ninjaTraining(vector<vector<int>>& matrix) {
+         
+         int n = matrix.size();
+         int m = 3;
+
+         if (n == 1) return max(max(matrix[0][0], matrix[0][1]), matrix[0][2]); // max takes only two arguments
+         
+         vector<vector<int>> dp(n, vector<int>(m, 0));
+         dp[0][0] = matrix[0][0];
+         dp[0][1] = matrix[0][1];
+         dp[0][2] = matrix[0][2];
+
+         for(int i = 1; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                int maxPrev = 0;
+                for(int k = 0; k < m; k++) {
+                    if(k != j) {
+                        maxPrev = max(dp[i-1][k], maxPrev);
+                    }
+                }
+
+                dp[i][j] = matrix[i][j] + maxPrev;
+            }
+         }
+
+         return max(max(dp[n-1][0], dp[n-1][1]), dp[n-1][2]);
+    }
+};
